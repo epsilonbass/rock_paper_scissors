790a3a9fa96dee94bcaba611187b312ac219b49f
epsilonbass <epsilonbass@gmail.com>
2021-01-18 12:24:38 -0500

    added playerPlay function

--- rpcscript.js
+++ rpcscript.js
@@ -1,10 +1,22 @@
 // computer randomly selects one of three parameters, rock paper or scissors
-function computerPlay(selection) {
+function playerPlay(playerSelection) {
+    playerSelection = prompt("Rock/Paper/Scissors?").toLowerCase();
+    if (playerSelection === "rock" ||
+        playerSelection === "paper" ||
+        playerSelection === "scissors"){
+        document.getElementById("playerdemo").innerHTML = playerSelection;
+    } else {
+        document.getElementById("playerdemo").innerHTML = "<h1>You are an idiot...</h1>";
+    }
+    
+}
+function computerPlay(computerSelection) {
     choice = ["rock", "paper", "scissor"];
     //selection turns choice into numbers 0-2 and randomly selects one number 
-    selection = choice[Math.floor(Math.random() * choice.length)];
-    document.getElementById("demo").innerHTML = selection;
+    computerSelection = choice[Math.floor(Math.random() * choice.length)];
+    document.getElementById("demo").innerHTML = computerSelection;
 }
+
 
 
 // prompt user to enter rock paper scissor
